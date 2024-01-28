1c81980e5b15b6b4f5d608d68cc2a6716e1bfab5
zachary chapman <zach.chapman24@gmail.com>
2024-01-28 17:47:01 -0500

    wrtie to file worked, fixed setshape data info to show what user input, fixed square

--- index.js
+++ index.js
@@ -1,48 +1,58 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const setShape = require('./lib/setShape.js')
+const fileName = "logo.svg"
+
 
 //questions that will be asked to make the logo
 const questions = [
   {
     type: "input",
-    message: "Enter three characters you would like for the LOGO",
-    name: "characters",
-    
+    message: "Enter three characters you would like for the LOGO:",
+    name: "text",
+    validate: function (input) {
+      // Check if the input has exactly three letters
+      return /^[a-zA-Z]{3}$/.test(input)
+        ? true
+        : "Please enter exactly three letters.";
+    },
   },
 
   {
     type: "input",
-    message: "What color would you like the text to be ",
+    message: "What color would you like the text to be: ",
     name: "textColor",
   },
 
   {
     type: "list",
-    choices: ['circle', 'triangle', 'square'],
-    message: "Select the shape you would like",
+    choices: ["Circle", "Triangle", "Square"],
+    message: "Select the shape you would like: ",
     name: "shape",
   },
 
   {
     type: "input",
-    message: "What color would you like the shape to be ",
+    message: "What color would you like the shape to be: ",
     name: "shapeColor",
   },
 ];
 
-
-
-function writeToFile(fileName, data) {
-  fs.writeFile(fileName, data, (err) => {
-    if (err) throw err;
-    
-  });
+function createLogo(response, fileName) {
+  const svg = setShape(response);
+  fs.writeFile(fileName, svg, () => console.log("Generated " + fileName));
 }
 
+// initialize, ask questions then createLogo using responses, catch any errors
 function init() {
-  inquirer.prompt(questions).then((response) => {
-   
-  });
+  inquirer
+    .prompt(questions)
+    .then((response) => {
+      createLogo(response, fileName);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 init();
